4291a088f1e8f547a9e4cf985f1aa6a3eafa7922
juan esteban katz <jestebankatz@gmail.com>
2019-06-12 18:18:15 -0500

    Esquelo de la aplicación con rutas y componentes iniciales.

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {ROUTES} from './app.routes';
+
+import { HttpClientModule } from '@angular/common/http';
+
+//rutas de la aplicacion
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES , { useHash:true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
